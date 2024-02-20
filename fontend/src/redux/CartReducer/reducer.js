c17abdea29fcebf72d4504969c88bb8eb858c8c1
subha20032007 <meunknown466@gmail.com>
2024-02-21 00:10:28 +0530

    Ad

--- fontend/src/redux/CartReducer/reducer.js
+++ fontend/src/redux/CartReducer/reducer.js
@@ -1,9 +1,12 @@
+import { ADD_TO_CART } from "./actionType";
+
 const inititalState={
     cart:[]
 }
-const reducer=(state=inititalState,{type,payload}){
+const reducer=(state=inititalState,{type,payload})=>{
     switch(type){
-        
+        case ADD_TO_CART:
+            return {...state,cart:[...state.cart,payload]}
         default:
             return state;
     }
--- fontend/src/redux/store.js
+++ fontend/src/redux/store.js
@@ -1,8 +1,11 @@
-import { reducer as userReducer } from "./UserReducer/reducer"
+import { reducer as authReducer } from "./AuthReducer/reducer"
+import {reducer as productReducer} from "./ProductReducer/reducer"
+import {reducer as cartReducer} from "./CartReducer/reducer"
 import {applyMiddleware, combaineReducer,legacy_createStore} from "redux"
 import thunk from "redux-thunk"
 const rootreducer=combaineReducer({
-userReducer,
-
+authReducer,
+productReducer,
+cartReducer
 })
 export const store=legacy_createStore(rootreducer,applyMiddleware(thunk))
